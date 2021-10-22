c9206116131f576b71b33a173511c66cd50500cc
Initial Commit <gorguindong@hotmail.fr>
2021-10-22 08:56:17 +0200

    Form validation support

--- src/main/java/com/djiman/tacoCloud/controller/DesignTacoController.java
+++ src/main/java/com/djiman/tacoCloud/controller/DesignTacoController.java
@@ -8,10 +8,12 @@ import com.djiman.tacoCloud.model.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,7 +25,10 @@ import java.util.stream.Collectors;
 public class DesignTacoController {
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "redirect:design";
+        }
         return "redirect:/orders/current";
     }
 
